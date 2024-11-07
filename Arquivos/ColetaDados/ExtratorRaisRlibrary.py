a70f0cf69e7884778e22e7d8d98d2602cf0964b4
AraujoFer26 <fernando.araujo2@hotmail.com>
2024-11-06 22:00:04 -0300

    ExtratorRaisRlibrary
    Captura dos dados da RAIS via RStudio.

---
+++ Arquivos/ColetaDados/ExtratorRaisRlibrary.py
@@ -0,0 +1,29 @@
+# No RStudio instalar as seguintes bibliotecas:
+# install.packages("remotes")
+# remotes::install_github("ManuelHentschel/vscDebugger")
+
+# Instalar o R no marketplacer do Visual Studio Code
+# R, R Tools e R Debugger
+
+# Instalar e carregar os pacotes necessários
+install.packages("readr")
+library(readr)
+
+# Baixar o arquivo .7z
+download.file("ftp://ftp.mtps.gov.br/pdet/microdados/NOVO%20CAGED/2024/202401/CAGEDMOV202401.7z",
+              destfile = "C:/Users/ferna/Downloads/CAGEDMOV202401.7z",
+              mode = "wb")
+
+# Para descompactar o arquivo .7z, você precisará do pacote 'archive' ou de uma ferramenta externa como o 7-Zip.
+# Se você tiver o pacote 'archive', instale e use o seguinte código para descompactar:
+
+install.packages("archive")
+library(archive)
+
+# Instale o pacote R.utils se ainda não estiver instalado
+install.packages("R.utils")
+library(R.utils)
+
+# Extraia o arquivo .7z na mesma pasta
+archive_extract("C:/Users/ferna/Downloads/CAGEDMOV202401.7z", dir = "C:/Users/ferna/Downloads")
+
